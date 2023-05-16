21f67e31a524c46c36b6d467cf792c4f5c323ecb
profbreno <brenod123@gmail.com>
2023-05-16 19:42:58 -0300

    Create fibonnati.py

---
+++ Lógica de Programação/2023/2023.1-GTI/Aula 4/fibonnati.py
@@ -0,0 +1,12 @@
+# Faça um programa para calcular a série de
+# Fibonacci para um número informado pelo usuário,
+# sendo F(0) = 0, F(1) = 1 e F(n)= F(n-1)+F(n-2)
+
+num = int(input("Digite um número"))
+a = 0
+b = 1
+for i in range(num):
+    print(a)
+    c = a + b
+    a = b
+    b = c
